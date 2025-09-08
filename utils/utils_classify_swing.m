bd1074b9c8210edcd51072407c0b43ae27e3b1a4
unknown <chrisjdallmann@gmail.com>
2025-09-08 17:00:19 +0200

    Update padding for multiple features

--- utils/utils_classify_swing.m
+++ utils/utils_classify_swing.m
@@ -7,18 +7,17 @@ function target_frames = utils_classify_swing(x,net,sliding_window)
 
 % Author: Chris J. Dallmann
 % Affiliation: University of Wuerzburg
-% Last revision: 07-October-2024
+% Last revision: 08-September-2025
 
 % ------------- BEGIN CODE ------------- 
 
-%sliding_window = 20; % Same length as window used for training the network
 target_score_threshold = 0.96; 
 
 % Load LSTM network
 load(net);
 
 % Add padding to avoid edge artifacts
-x_padded = [x, zeros(1,sliding_window)];
+x_padded = [x, zeros(size(x,1),sliding_window)];
 
 % Slide over x to generate test data  
 x_test = {};
@@ -43,29 +42,6 @@ target_frames(target_scores(target_frames)<target_score_threshold) = [];
 target_frames = target_frames+round(sliding_window/2);
 
 % Remove target frames in padded region
-target_frames(target_frames>numel(x)) = [];
-
-
-% % When using a single network for onset and offset classification
-% target_scores = scores(:,2:3);
-% 
-% % Find frames whith best scores  
-% target_frames = islocalmax(target_scores,'MinProminence',0.5); 
-% target_frames = double(target_frames);
-% 
-% swing_onsets = find(target_frames(:,2)>0);
-% swing_offsets = find(target_frames(:,1)>0);
-% 
-% % Threshold target frames
-% swing_onsets(target_scores(swing_onsets,2)<target_score_threshold) = [];
-% swing_offsets(target_scores(swing_offsets,1)<target_score_threshold) = [];
-% 
-% % Shift classifications by half the sliding window
-% swing_onsets = swing_onsets+round(sliding_window/2);
-% swing_offsets = swing_offsets+round(sliding_window/2);
-% 
-% % Remove target frames in padded region
-% swing_onsets(swing_onsets>numel(x)) = [];
-% swing_offsets(swing_offsets>numel(x)) = [];
+target_frames(target_frames>size(x,2)) = [];
 
 end
